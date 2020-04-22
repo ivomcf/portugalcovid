ea720102614f582b4240a2c23035e8b60fb261ae
ivomcf <32466992+ivomcf@users.noreply.github.com>
2020-04-22 09:37:34 +0100

    3 atividades criadas para os casos.

--- app/src/main/java/pt/ipg/portugalcovid/CasesActivity.java
+++ app/src/main/java/pt/ipg/portugalcovid/CasesActivity.java
@@ -2,13 +2,35 @@ package pt.ipg.portugalcovid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CasesActivity extends AppCompatActivity {
+
+    private Button buttonConfirmed;
+    private Button buttonSuspected;
+    private Button buttonRecovered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cases);
+
+        buttonConfirmed = findViewById(R.id.buttonConfirmados);
+        buttonConfirmed.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+                moveToConfirmedActivity();
+            }
+        });
+    }
+
+    private void moveToConfirmedActivity(){
+
+        Intent intent = new Intent(CasesActivity.this, ConfirmedActivity.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/pt/ipg/portugalcovid/RecoveredActivity.java
@@ -0,0 +1,14 @@
+package pt.ipg.portugalcovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecoveredActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recovered);
+    }
+}

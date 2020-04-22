55a7a578266c49077273c965c3c97e31d19d1da1
ivomcf <32466992+ivomcf@users.noreply.github.com>
2020-04-22 08:44:43 +0100

    Initial Commit. Layout + abrir nova atividade com botão.

---
+++ app/src/main/java/pt/ipg/portugalcovid/MainActivity.java
@@ -0,0 +1,34 @@
+package pt.ipg.portugalcovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonSuporte;
+    private Button buttonCases;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonSuporte = findViewById(R.id.buttonSupport);
+        buttonSuporte.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+                moveToSupportActivity();
+            }
+        });
+    }
+
+    private void moveToSupportActivity(){
+
+        Intent intent = new Intent(MainActivity.this, SupportActivity.class);
+        startActivity(intent);
+    }
+}

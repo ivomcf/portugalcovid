e615fe117d440382efe229562fda7231557f401b
ivomcf <32466992+ivomcf@users.noreply.github.com>
2020-04-27 11:24:15 +0100

    -Ajuda e suporte +Dados e validação dos mesmos

--- app/src/main/java/pt/ipg/portugalcovid/SupportActivity.java
+++ app/src/main/java/pt/ipg/portugalcovid/SupportActivity.java
@@ -2,7 +2,10 @@ package pt.ipg.portugalcovid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SupportActivity extends AppCompatActivity {
 
@@ -10,5 +13,46 @@ public class SupportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_support);
+    }
+
+    public void sendMessage(View view) {
+
+        EditText editTextNome = (EditText) findViewById(R.id.editTextNome);
+        EditText editTextTelefone = (EditText) findViewById(R.id.editTextTelefone);
+        EditText editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        EditText editTextIdade = (EditText) findViewById(R.id.editTextIdade);
+
+        String nome = editTextNome.getText().toString();
+        String telefone = editTextTelefone.getText().toString();
+        String email = editTextEmail.getText().toString();
+
+        String StrIdade = editTextIdade.getText().toString();
+        int idade=-1;
+        try {
+            idade = Integer.parseInt(StrIdade);
+        } catch (NumberFormatException e){
+            e.printStackTrace();
+        }
+
+        if (nome.length() == 0) {
+            editTextNome.setError("Nome Obrigatorio!");
+            editTextNome.requestFocus();
+            return;
+        }
+
+        if (telefone.length() == 0) {
+            editTextTelefone.setError("Telefone Obrigatorio!");
+            editTextTelefone.requestFocus();
+            return;
+        }
+
+        if (email.length() == 0) {
+            editTextEmail.setError("Telefone Obrigatorio!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        Intent intent = new Intent(this, DisplayMessagaActivity.class); //TODO Nova activity
+        startActivity(intent);
     }
 }
